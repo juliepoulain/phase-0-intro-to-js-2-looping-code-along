662feda94772125d6194449728d9a71adf056e8a
Julie Poulain <juliempoulain@gmail.com>
2024-02-27 12:26:53 -0700

    added writeCards and countDown functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// for ([initialization]; [condition]; [iteration]) {[loop body]}
+
+function writeCards(names, event){
+    const messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+// while ([condition]) {[loop body]}
+
+function countDown(positiveInteger){
+    while (positiveInteger >= 0) {
+        console.log(positiveInteger--)
+    }
+}
